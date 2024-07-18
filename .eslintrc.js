dcc8843e6a2fd501aff2909bd927cf0e4f90102a
wataemon0618 <watatataru0618@gmail.com>
2024-07-18 22:28:08 +0900

    section6までの下準備完了

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    jquery: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'none',
+      },
+    ],
+    'no-console': ['off'],
+  },
+};
---
+++ app.js
@@ -0,0 +1,3 @@
+const PORT = process.env.PORT;
+// console.log(PORT);
+console.log(`Application listening at ${PORT}`);
